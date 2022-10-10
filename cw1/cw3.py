c544c64bfa105c4297e0d0c8b00e8f570dfc3fef
galowicz <krzysztofg4163@protonmail.com>
2022-10-10 14:38:59 +0200

    add cw3

---
+++ cw1/cw3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+	kod = "1234"
+	string = input("podaj kod:")
+	if kod == string:
+		print("kod jest poprawny")
+	else:
+		print("kod jest niepoprawny")

d65366d97afd8c354e4bf13ece8613c5e61c710c
galowicz <krzysztofg4163@protonmail.com>
2022-10-17 15:16:21 +0200

    update parser, change suffix instead adding

--- cw2/cw3.py
+++ cw2/cw3.py
@@ -2,14 +2,23 @@
 from pathlib import Path
 from PIL import Image
 import sys
-def jpg2png(file):
+import argparse
+
+def jpg2png(file:Path):
 	jpg = Image.open(file)
-	jpg.save(file+".png")
+	jpg.save(file.with_suffix(".png"))
 	return 0
 
 if __name__ == '__main__':
-	for i in range(1,len(sys.argv)):
-		# if not jpg2png(sys.argv[i]):
-		# 	print("failed to convert file:", sys.argv[i])
-		jpg2png(sys.argv[i])
+	# for i in range(1,len(sys.argv)):
+	# 	# if not jpg2png(sys.argv[i]):
+	# 	# 	print("failed to convert file:", sys.argv[i])
+	# 	jpg2png(Path(sys.argv[i]))
 
+	parser = argparse.ArgumentParser(description="Convert jpg file to png")
+	parser.add_argument('file',metavar="file", type=str,help="file to convert",nargs='+')
+
+	args = parser.parse_args()
+	
+	for i in range(len(vars(args)['file'])):
+		jpg2png(Path(vars(args)['file'][i]))
